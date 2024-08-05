b22a241368980c1517d0bc072b62c87df5fb4d80
Bikash Shaw <1985.bikash.shaw@gmail.com>
2024-08-05 09:35:59 +0530

    Initial commit with all packages

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+function App() {
+  return (
+    <div>
+      <h1>Welcome to VisionaryInventor</h1>
+      <h3>Name: {import.meta.env.VITE_NAME}</h3>
+    </div>
+  );
+}
+
+export default App;
